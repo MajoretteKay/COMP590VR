937f3d5288f73e8208b6d1d606aedc9c84b99f0e
MajoretteKay <keolecki@gmail.com>
2020-03-03 01:23:03 -0500

    trap
    a6

---
+++ Assets/Trap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Collectible cube;
+    public Rigidbody ground;
+    public Rigidbody table;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cube.active != null) {
+            ground.useGravity = true;
+            ground.isKinematic= false;
+            table.useGravity = true;
+            table.isKinematic= false;
+            
+        }
+    }
+}
